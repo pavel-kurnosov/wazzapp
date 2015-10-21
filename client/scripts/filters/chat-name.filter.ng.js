bca93e43f618057341eb3053a111c46f2d0c189c
pavel kurnosov <pvl.kurnosov@gmail.com>
2015-10-22 00:47:43 +0200

    fix last step

--- client/scripts/filters/chat-name.filter.ng.js
+++ client/scripts/filters/chat-name.filter.ng.js
@@ -1,15 +1,15 @@
-//angular
-//  .module('wazzapp')
-//  .filter('chatName', chatName);
-//
-//function chatName() {
-//  return function (chat) {
-//    if (!chat) return;
-//
-//    var otherId = _.without(chat.userIds, Meteor.userId())[0];
-//    var otherUser = Meteor.users.findOne(otherId);
-//    var hasName = otherUser && otherUser.username;
-//
-//    return hasName ? otherUser.username : chat.name || 'NO NAME';
-//  }
-//}
+angular
+  .module('wazzapp')
+  .filter('chatName', chatName);
+
+function chatName() {
+  return function (chat) {
+    if (!chat) return;
+
+    var otherId = _.without(chat.userIds, Meteor.userId())[0];
+    var otherUser = Meteor.users.findOne(otherId);
+    var hasName = otherUser && otherUser.username;
+
+    return hasName ? otherUser.username : chat.name || 'NO NAME';
+  }
+}
