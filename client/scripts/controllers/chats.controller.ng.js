6077f76bc3cc968fb50986e233a807b71404d977
pavel kurnosov <pvl.kurnosov@gmail.com>
2015-10-22 00:45:20 +0200

    step7

--- client/scripts/controllers/chats.controller.ng.js
+++ client/scripts/controllers/chats.controller.ng.js
@@ -2,11 +2,26 @@ angular
   .module('wazzapp')
   .controller('ChatsCtrl', ChatsCtrl);
 
-function ChatsCtrl($scope) {
+function ChatsCtrl($scope, $ionicModal, $meteor) {
 
   $scope.chats = $scope.$meteorCollection(Chats, false);
 
   $scope.remove = function(chat) {
+    //$meteor.call('removeChat', chat._id);
     $scope.chats.remove(chat);
   };
+  //
+  //$ionicModal.fromTemplateUrl('client/templates/new-chat.ng.html', {
+  //  scope: $scope
+  //}).then(function (modal) {
+  //  $scope.modal = modal;
+  //});
+  //
+  //$scope.$on('$destroy', function () {
+  //  $scope.modal.remove();
+  //});
+  //
+  //$scope.openNewChatModal = function () {
+  //  $scope.modal.show();
+  //}
 }
